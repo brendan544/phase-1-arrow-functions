dbdaaf1101ac9436aac063f04b5a4fd272c290a8
brendan544 <brendan.gwer@student.moringaschool.com>
2024-07-08 16:55:52 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,19 @@
+let a = 2000
+let b = 10
+const divide = function(a,b) {
+                return a/b
+              }
+const result = divide(2000,100)
+console.log(result);
+
+const square = (x) => {
+   return x*x
+}
+const answer = square(2)
+ 
+const add = (parameter1, parameter2) => {
+    return parameter1 + parameter2;
+}
+
+const outcome = add(3, 4);
+console.log(outcome); 
